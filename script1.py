652de9c34863a2f884a70edd25c36f922e22a316
mohamed-hisham-swidan <mohamed.hisham.swidan@gmail.com>
2026-04-10 13:03:26 -0700

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,15 @@
+
+def funn() :
+ if  __name__ == '__main__':
+    i = None
+ else :
+    return __name__
+
+
+    print()
+funn()
+def main():
+    from script2 import my_real_name
+    name = my_real_name()
+    print(f"this is project real name is {__file__}")
+main()
---
+++ script2.py
@@ -0,0 +1,5 @@
+def my_real_name():
+  from script1 import funn
+  name = funn()
+  return name
+
